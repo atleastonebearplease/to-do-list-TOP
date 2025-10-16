9f54c5d06b61d3dddcc06341dea613ec1efa6912
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-10-16 11:52:37 -0500

    feat: Add IDService
    -Create new idService file
    -Created some ID Service functionality for only tasks
    -Added testing code to main JS file

---
+++ src/idService.js
@@ -0,0 +1,30 @@
+/*
+    This module allows use to use a hierarchical ID. In other words, a task ID contains the ID of
+    all of its ancestors. Say you have parent1.ID = 10 and child1 ID = 20; The ID would be 10-20.
+    Simple enough.
+*/
+
+export class IDService {
+    static currentTaskID = 0;
+    static currentProjectID = 0;
+
+    static getNewTaskID(parentID = "") {
+        if(parentID){
+            this.currentTaskID += 1;
+            return parentID + "-" + this.currentTaskID;
+        } else {
+            this.currentTaskID += 1;
+            return this.currentTaskID;
+        }
+    }
+
+    static getIDLayer(layer, ID) {
+        let idLayers = ID.split("-");
+
+        if(layer >= idLayers.length){
+            return "ERROR: ID does not have this many layers";
+        } else {
+            return idLayers[layer];
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { Task } from "./task.js";
+import { IDService } from "./idService.js";
 
 import { format } from 'date-fns';
 
@@ -7,14 +8,26 @@ console.log("Hello World");
 
 console.log(format(new Date(), "yyyy-MM-dd' at 'HH:mm:ss.SSS"));
 
+/*
+    Test out Task functionality
+*/
 let task = new Task("Get Milk", "Go to Walmart and grab some milk.");
-
 console.log(task.title);
 
 task.complete();
-
 console.log("This task was completed on " + task.getCompletionDate());
 
 task.unComplete();
-
 console.log("This task was completed on " + task.getCompletionDate());
+
+
+/*
+    Test out IDService
+*/
+
+console.log(IDService.getNewTaskID() );
+console.log(IDService.getNewTaskID("10"));
+
+let layeredID = IDService.getNewTaskID("10-30-10");
+
+console.log(IDService.getIDLayer(1, layeredID) ); //Should be 30
