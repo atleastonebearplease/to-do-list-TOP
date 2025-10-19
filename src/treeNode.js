441f208038da7b8cd515e488462d0c14faad3917
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2025-10-19 05:42:01 -0500

    feat: Commit working files for travel

---
+++ src/treeNode.js
@@ -0,0 +1,20 @@
+export class TreeNode {
+    parent;
+    children;
+    data;
+    id = 0;
+
+    constructor(data) {
+        this.data = data;
+    }
+
+    setRoot(node) {
+        this.parent = node;
+        node.addChild(this);//TODO make sure this actually is correct
+    }
+
+    addChild(node) {
+        this.children.push(node);
+        node.parent = this;
+    }
+}
